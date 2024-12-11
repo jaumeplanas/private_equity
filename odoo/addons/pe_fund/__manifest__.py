9bd020768bac744d8a8ca287453c7498d9f47ca1
Jaume Planas <jaumeplan@gmail.com>
2024-12-11 18:14:28 +0100

    ✨ feat: added module category

--- odoo/addons/pe_fund/__manifest__.py
+++ odoo/addons/pe_fund/__manifest__.py
@@ -1,8 +1,14 @@
 {
     "name": "Private Equity Fund",
+    "summary": "",
     "author": "Jaume Planas",
     "website": "https://www.jaumeplanas.cat",
     "maintainers": ["jaumeplanas"],
+    "category": "Private Equity",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
+    "depends": [],
+    "data": [
+        "security/module_category.xml",
+    ],
 }
--- odoo/addons/pe_fund/readme/DESCRIPTION.md
+++ odoo/addons/pe_fund/readme/DESCRIPTION.md
@@ -1,1 +1,6 @@
-This module includes several addons to create a basic Private Fund Management framework.
+This module includes several data models and confgurations common
+to the other Private Fund Management features.
+
+It includes:
+
+* A new Module Category specific to this vertical.
