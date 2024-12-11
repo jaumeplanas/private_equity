de95798fe4b429a5f896fcd81c4acd82e10019a5
Jaume Planas <jaumeplan@gmail.com>
2024-12-11 09:35:51 +0100

    🎉 init: initial adjustments

--- odoo/addons/pe_base/__manifest__.py
+++ odoo/addons/pe_base/__manifest__.py
@@ -1,11 +1,8 @@
 {
-    "name": "Fund Management",
+    "name": "Private Equity Base",
     "author": "Jaume Planas",
     "website": "https://www.jaumeplanas.cat",
     "maintainers": ["jaumeplanas"],
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
-    "depends": [
-        "account_usability",
-    ],
 }
